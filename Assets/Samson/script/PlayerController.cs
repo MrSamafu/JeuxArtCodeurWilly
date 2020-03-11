c845b49d68a75ad9c154be9e31c2f465560c80c0
Samson <51507673+MrSamafu@users.noreply.github.com>
2020-03-11 22:01:35 +0100

    Creation d'unpersonnage en vu FPS(non fini) + Ajout serveur Mirror(online gaming)
    J'ai créer un Tic Tac armé qui ce déplace.
    J'ai aussi télécharger un asset remplacent le systeme de jeux en ligne de Unity qui est devenu obsolete.

---
+++ Assets/Samson/script/PlayerController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed = 10f;
+    public float lookSensitivity = 3f;
+
+    private PlayerMotor motor;
+
+    private void Start()
+    {
+        motor = GetComponent<PlayerMotor>();
+    }
+
+    private void Update()
+    {
+        float _xMov = Input.GetAxisRaw("Horizontal");
+        float _zMov = Input.GetAxisRaw("Vertical");
+
+        Vector3 _moveHorizontal = transform.right * _xMov;
+        Vector3 _moveVertical = transform.forward * _zMov;
+
+        Vector3 _velocity = (_moveHorizontal + _moveVertical).normalized * speed; //regarder doc "normalized"
+
+        motor.Move(_velocity);
+
+        float _yRot = Input.GetAxisRaw("Mouse X");
+
+        Vector3  _rotation= new Vector3(0, _yRot, 0) * lookSensitivity;
+
+        motor.Rotate(_rotation);
+
+        float _xRot = Input.GetAxisRaw("Mouse Y");
+
+        Vector3 _cameraRotation = new Vector3(_xRot, 0, 0) * lookSensitivity;
+
+        motor.RotateCamera(_cameraRotation);
+
+
+    }
+}
---
+++ Assets/Samson/script/PlayerMotor.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMotor : MonoBehaviour
+{
+    private Vector3 velocity;
+    private Vector3 rotation;
+    private Vector3 cameraRotation;
+
+    public Camera cam;
+
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    public void Move(Vector3 _velocity)
+    {
+        velocity = _velocity;
+    }
+    private void FixedUpdate()
+    {
+        PerformMovement();
+        PerformRotation();
+    }
+    private void PerformMovement()
+    {
+        if(velocity != Vector3.zero)
+        {
+            rb.MovePosition(rb.position + velocity * Time.fixedDeltaTime);
+        }
+    }
+    public void Rotate(Vector3 _rotation)
+    {
+        rotation = _rotation;
+    }
+    public void RotateCamera(Vector3 _cameraRotation)
+    {
+        cameraRotation = _cameraRotation;
+    }
+    private void PerformRotation()
+    {
+        rb.MoveRotation(rb.rotation * Quaternion.Euler(rotation));
+        cam.transform.Rotate(-cameraRotation);
+    }
+
+
+}
