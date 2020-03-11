5a9140209ec275de89cf9d3d841be09dbad09bcf
Samson <51507673+MrSamafu@users.noreply.github.com>
2020-03-12 00:06:54 +0100

    Creation d'un Hub Online dans le labo
    Gestion des composant à desactiver en cas de multijoueurs

---
+++ Assets/Samson/Script/PlayerSetup.cs
@@ -0,0 +1,26 @@
+﻿using Mirror;
+using UnityEngine;
+
+public class PlayerSetup : NetworkBehaviour
+{
+
+    public Behaviour[] componentsToDisable;
+
+    private void Start()
+    {
+        if (!isLocalPlayer)
+        {
+            //boucle qui desactive les components des autre joueurs.
+            for(int i = 0; i < componentsToDisable.Length; i++)
+            {
+                componentsToDisable[i].enabled = false;
+
+            }
+        }
+        else
+        {
+            Camera.main.gameObject.SetActive(false);
+            
+        }
+    }
+}
