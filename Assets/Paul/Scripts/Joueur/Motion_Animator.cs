b425a19a67bfc6b68ac9dff4f8ea5285bb5e9a48
Paul Lefebvre <paul.lefebvre@viacesi.fr>
2020-04-16 02:37:03 +0200

    ajout anim gauche droite
    ajout anim pour tourner en marchant

---
+++ Assets/Paul/Scripts/Joueur/Motion_Animator.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Motion_Animator : MonoBehaviour
+{
+
+    public Animator anim;
+
+
+    void Update()
+    {
+        
+        anim.SetFloat("horizontal", Input.GetAxis("Horizontal"));
+        anim.SetFloat("vertical", Input.GetAxis("Vertical"));
+
+    }
+}
