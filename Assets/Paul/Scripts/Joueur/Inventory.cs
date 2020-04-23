9271c1cc7d1e48db4a94bb9882736a66f8d07faa
Paul Lefebvre <paul.lefebvre@viacesi.fr>
2020-04-23 04:42:11 +0200

    FIN DE SESSION
    Fin de session (ajout de systeme de picking object dans la bar de naviguation des items)

--- Assets/Paul/Scripts/Joueur/Inventory.cs
+++ Assets/Paul/Scripts/Joueur/Inventory.cs
@@ -21,7 +21,15 @@ public class Inventory : MonoBehaviour
     public GameObject slotsBar;
     public GameObject slotsUI;
 
+    public int activatedSlot = 1;
+    public GameObject activeSlotCanvas;
+    public GameObject itemTaked;
+    public Transform rightHand;
+    private bool TakingObject;
+
     void Start(){
+        itemTaked = null;
+
         allSlotsInventory = 16;
         allSlotsBar = 5;
         slot = new GameObject[allSlotsInventory];
@@ -46,7 +54,7 @@ public class Inventory : MonoBehaviour
     }
 
 
-    void Update(){
+    public void Update(){
         
         if(Input.GetButtonDown("Inventory"))
             inventoryEnabled = !inventoryEnabled;
@@ -59,7 +67,133 @@ public class Inventory : MonoBehaviour
         }
 
 
+        //-----------GESTION DE LA BAR--------------
+        if (Input.GetKeyDown (KeyCode.Alpha1)){
+
+            if(itemTaked != null){
+                rightHand.GetChild(0).SetParent(slotBar[0].transform, false);
+            }
+
+            itemTaked = slotBar[0].GetComponent<Slot>().item;
+            activatedSlot = 1;
+        }
+
+        if (Input.GetKeyDown (KeyCode.Alpha2)){
+            itemTaked = slotBar[1].GetComponent<Slot>().item;
+            activatedSlot = 2;
+            TakingObject = true;
+        }
+
+        if (Input.GetKeyDown (KeyCode.Alpha3)){
+            itemTaked = slotBar[2].GetComponent<Slot>().item;
+            activatedSlot = 3;
+            TakingObject = true;
+        }
+
+        if (Input.GetKeyDown (KeyCode.Alpha4)){
+            itemTaked = slotBar[3].GetComponent<Slot>().item;
+            activatedSlot = 4;
+            TakingObject = true;
+        }
+
+        if (Input.GetKeyDown (KeyCode.Alpha5)){
+            itemTaked = slotBar[4].GetComponent<Slot>().item;
+            activatedSlot = 5;
+            TakingObject = true;
+        }
+
+
+
+        //GESTION TAKE DE L'ITEM DANS LA BAR
+        //1
+        if(itemTaked != null){
+        if (activatedSlot == 1){
+            slotBar[0].GetComponent<Image>().color = new Color32(0,106,171,100);
+
+                
+            itemTaked.transform.SetParent(rightHand.transform, false);
+
+            itemTaked.SetActive(true);
+            //itemTaked.transform.position = new Vector3(0, 0, 0);
+            //itemTaked.transform.rotation = Quaternion.Euler(0,0,0);
+
+
+            
+        }else{
+            slotBar[0].GetComponent<Image>().color = new Color32(255,255,255,100);
+            itemTaked.SetActive(false);
+        }//-----------------
+
+        //2
+        if (activatedSlot == 2){
+            slotBar[1].GetComponent<Image>().color = new Color32(0,106,171,100);
+            itemTaked = slotBar[1].GetComponent<Slot>().item;
+
+            if(TakingObject){
+                itemTaked.transform.SetParent(rightHand.transform, false);
+                itemTaked.SetActive(true);
+
+                itemTaked.transform.position = new Vector3(0, 0, 0);
+                itemTaked.transform.rotation = Quaternion.Euler(0,0,0);
+                TakingObject = false;
+            }
+
+        }//------------------
+
+        //3
+        if (activatedSlot == 3){
+            slotBar[2].GetComponent<Image>().color = new Color32(0,106,171,100);
+            itemTaked = slotBar[2].GetComponent<Slot>().item;
+
+            if(TakingObject){
+                itemTaked.transform.SetParent(rightHand.transform, false);
+                itemTaked.SetActive(true);
+
+                itemTaked.transform.position = new Vector3(0, 0, 0);
+                itemTaked.transform.rotation = Quaternion.Euler(0,0,0);
+                TakingObject = false;
+            }
+
+        }//---------------
+
+        //4
+        if (activatedSlot == 4){
+            slotBar[3].GetComponent<Image>().color = new Color32(0,106,171,100);
+            itemTaked = slotBar[3].GetComponent<Slot>().item;
+
+            if(TakingObject){
+                itemTaked.transform.SetParent(rightHand.transform, false);
+                itemTaked.SetActive(true);
+
+                itemTaked.transform.position = new Vector3(0, 0, 0);
+                itemTaked.transform.rotation = Quaternion.Euler(0,0,0);
+                TakingObject = false;
+            }
+
+        }//---------------------
+
+        //5
+        if (activatedSlot == 5){
+            slotBar[4].GetComponent<Image>().color = new Color32(0,106,171,100);
+            itemTaked = slotBar[4].GetComponent<Slot>().item;
+
+            if(TakingObject){
+                itemTaked.transform.SetParent(rightHand.transform, false);
+                itemTaked.SetActive(true);
+
+                itemTaked.transform.position = new Vector3(0, 0, 0);
+                itemTaked.transform.rotation = Quaternion.Euler(0,0,0);
+                TakingObject = false;
+            }
+
+        }//----------------------
+        
+        }
+
+
+
     }
+
 
 
     public void AddItem(GameObject itemObject, int itemID, string itemType, string itemDescription, Sprite itemIcon){
