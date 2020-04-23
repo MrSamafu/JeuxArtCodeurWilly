0c84f13de6a9b6a1e1d5e4909725736be7330cc6
Paul Lefebvre <paul.lefebvre@viacesi.fr>
2020-04-23 22:00:01 +0200

    Réglages animations movement
    Grosse correction des bugs de mouvement (timing etc)
    ajout de la touche A ET Q pour aller a gauche (pas de jaloux)

---
+++ Assets/Paul/Scripts/Joueur/IdleToCrouch.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IdleToCrouch : MonoBehaviour
+{
+
+    public Animator animator;
+    float InputX;
+    public float InputY;
+
+    void Start()
+    {
+        animator = this.gameObject.GetComponentInChildren<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update(){
+        InputY = Input.GetAxis("Vertical");
+        InputX = Input.GetAxis("Horizontal");
+        animator.SetFloat("InputX", InputX);
+        animator.SetFloat("InputY", InputY);
+    }
+}
