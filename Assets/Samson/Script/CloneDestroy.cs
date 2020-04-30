dbe5cd2b7f5b8047b8fa7fab51a0b522487349ff
Samson <51507673+MrSamafu@users.noreply.github.com>
2020-04-30 21:13:11 +0200

    Add Explosive mine v1

---
+++ Assets/Samson/Script/CloneDestroy.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloneDestroy : MonoBehaviour
+{
+    public float timer = 4f;
+    void Start()
+    {
+        Destroy(gameObject, timer);
+    }
+}
