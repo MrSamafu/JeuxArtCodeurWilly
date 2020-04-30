c28176253c0dac78c77452faf8ad916776a674c4
Samson <51507673+MrSamafu@users.noreply.github.com>
2020-04-30 12:57:30 +0200

    start fire function

---
+++ Assets/Samson/Script/Shoot.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+    public float damage = 10f;
+    public float range = 100f;
+    public Cinemachine.CinemachineVirtualCamera cam;
+    public Transform gun;
+
+    // Update is called once per frame
+    void Update()
+    { 
+        Debug.DrawRay(gun.transform.position, gun.transform.forward, Color.red);
+        if (Input.GetButtonDown("Fire1"))
+        {
+           
+            Shooting();
+        } 
+    }
+    void Shooting()
+    {
+        RaycastHit hit;
+        if(Physics.Raycast(gun.transform.position, gun.transform.forward, out hit, range))
+        {
+            Debug.Log(hit.transform.name);
+        }
+    }
+}
